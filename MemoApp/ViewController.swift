cd7a828940b5440b4930ae8bc24b43245ab611fe
이 종원 <jong12em@naver.com>
2020-12-22 00:58:58 +0900

    [UI] 메모 목록 화면 디자인
    1. actionSheet 생성
    
    2. 정렬 버튼 생성
    
    3. 추가 버튼 생성
    
    4. collectionView 생성
    
    5. 정렬 버튼, actionSheet 연결

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -6,13 +6,68 @@
 //
 
 import UIKit
+import SnapKit
+import SVGKit
 
 class ViewController: UIViewController {
-
+    
+    let actionSheetSort:UIAlertController = UIAlertController(title: "정렬 방식", message: "선택한 방식으로 메모들이 정렬됩니다.", preferredStyle: .actionSheet)
+    let buttonSort:UIButton = UIButton()
+    let buttonAdd:UIButton = UIButton()
+    let collectionView:UICollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout.init())
+    
+    let imagePlus:SGVKImage = SVGKImage(named: "plus")
+    let imageSort:SGVKImage = SVGKImage(named: "sort")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let guide = self.view.safeAreaLayoutGuide.snp
         // Do any additional setup after loading the view.
+        actionSheetSort.addAction(UIAlertAction(title: "제목", style: .default, handler: nil))
+        actionSheetSort.addAction(UIAlertAction(title: "생성일", style: .default, handler: nil))
+        actionSheetSort.addAction(UIAlertAction(title: "수정일", style: .default, handler: nil))
+        actionSheetSort.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        self.view.addSubview(collectionView)
+        collectionView.backgroundColor = .black
+        collectionView.snp.makeConstraints { make in
+            make.trailing.equalTo(guide.trailing)
+            make.leading.equalTo(guide.leading)
+            make.top.equalTo(guide.top)
+            make.bottom.equalTo(0)
+        }
+        
+        self.view.addSubview(buttonSort)
+        //buttonSort.backgroundColor = .red
+        buttonSort.snp.makeConstraints { make in
+            make.leading.equalTo(guide.leading).offset(10)
+            make.top.equalTo(guide.top).offset(10)
+            make.width.equalTo(30)
+            make.height.equalTo(30)
+        }
+    
+        buttonSort.addTarget(self, action: #selector(onButtonActionSheetClicked), for: .touchUpInside)
+        
+        self.view.addSubview(buttonAdd)
+        buttonAdd.backgroundColor = .blue
+        buttonAdd.snp.makeConstraints { make in
+            make.trailing.equalTo(guide.trailing).offset(-10)
+            make.top.equalTo(guide.top).offset(10)
+            make.width.equalTo(30)
+            make.height.equalTo(30)
+        }
+        
+        
     }
+    
+    @objc
+    func onButtonActionSheetClicked(sender:UIButton){
+        if (!actionSheetSort.isBeingPresented){
+            present(actionSheetSort, animated: true, completion: nil)
+        }
+    }
+
 
 
 }
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MemoApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MemoApp
+  pod 'SnapKit', '~> 5.0.0'
+  pod 'SVGKit', :git => 'https://github.com/SVGKit/SVGKit.git', :branch => '2.x'
+
+  target 'MemoAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MemoAppUITests' do
+    # Pods for testing
+  end
+
+end
