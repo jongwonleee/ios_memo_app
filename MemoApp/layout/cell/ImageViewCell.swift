1ad442ffd27ac11396a4463aa425ed4546bbdb4b
이 종원 <jong12em@naver.com>
2020-12-29 00:32:11 +0900

    imageView cell UI 구성

---
+++ MemoApp/layout/cell/ImageViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  ImageViewCell.swift
+//  MemoApp
+//
+//  Created by 이종원 on 2020/12/29.
+//
+
+import UIKit
+
+class ImageViewCell: UICollectionViewCell {
+    public var imageView:UIImageView = UIImageView()
+    public var eraseButton:UIButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.addSubview(imageView)
+        self.contentView.addSubview(eraseButton)
+        
+        imageView.backgroundColor = .systemBlue
+        imageView.snp.makeConstraints{ make in
+            make.top.equalTo(8)
+            make.bottom.equalTo(8)
+            make.trailing.equalTo(8)
+            make.leading.equalTo(8)
+        }
+        
+        eraseButton.backgroundColor = .systemPink
+        eraseButton.snp.makeConstraints{make in
+            make.top.equalTo(2)
+            make.trailing.equalTo(2)
+            make.width.equalTo(12)
+            make.height.equalTo(12)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+    }
+}
