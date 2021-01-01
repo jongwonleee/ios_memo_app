53942fbb72af697462151f003b2c41d4540609a0
이 종원 <jong12em@naver.com>
2021-01-02 01:10:25 +0900

    [기능] memoDetail realm 연동
    1. scrollView에 uilabel 삽입
    
    2. uilabel에 메모 내용 표시
    
    3. view title에 메모 이름 표시

--- MemoApp/layout/vc/MemoDetailViewController.swift
+++ MemoApp/layout/vc/MemoDetailViewController.swift
@@ -12,6 +12,7 @@ import SnapKit
 class MemoDetailViewController: UIViewController {
     
     private var id:Int = -1
+    private var memo:MemoDao = MemoDao()
     public var memoId:Int
     {
         get{id}
@@ -19,13 +20,35 @@ class MemoDetailViewController: UIViewController {
         //TODO 나중에 숫자가 아니라 dao로 변경하기
         set(newVal){
             id = newVal
-            navigationItem.title = "\(newVal)"
+            memo = realm.objects(MemoDao.self).filter("id = \(id)").toArray()[0]
+            navigationItem.title = memo.title
+            textView.text = "\(memo.title)\n\(memo.content ?? "")"
         }
     }
     
-    private lazy var scrollView:UIScrollView = {
+    private let scrollView:UIScrollView = {
         let sv:UIScrollView = UIScrollView()
-        sv.backgroundColor = .systemRed
+        sv.isScrollEnabled = true
+        sv.backgroundColor = .white
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
+    
+    private let textView:UILabel = {
+        let tv:UILabel = UILabel()
+        tv.font = tv.font.withSize(20)
+        tv.numberOfLines = 0
+        tv.lineBreakMode = .byWordWrapping
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    private let scrollViewContent:UIStackView = {
+        let sv:UIStackView = UIStackView()
+        sv.axis = .vertical
+        sv.spacing = 8
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.backgroundColor = .white
         return sv
     }()
     
@@ -40,20 +63,40 @@ class MemoDetailViewController: UIViewController {
     }
     
     private func setUI(){
+        
+        
         self.view.addSubview(scrollView)
+        scrollView.addSubview(scrollViewContent)
+        scrollViewContent.addArrangedSubview(textView)
+
         scrollView.snp.makeConstraints{ make in
             make.top.equalTo(guide.top)
             make.bottom.equalTo(0)
             make.leading.equalTo(guide.leading)
             make.trailing.equalTo(guide.trailing)
         }
+        
+        scrollViewContent.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        
+        textView.snp.makeConstraints { make in
+            make.top.equalTo(8)
+            make.leading.equalTo(8)
+            make.trailing.equalTo(8)
+            make.height.greaterThanOrEqualTo(0)
+        }
+        
+        //scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: 2000)
     }
     
     @objc
     private func onButtonEditClicked(_ sender:UIButton) {
         let vc:MemoEditViewController = MemoEditViewController()
         vc.memoId = id
-        print("!")
         self.navigationController?.pushViewController(vc, animated: true)
     }
+
 }
